5d970b6167e2e0f0ab217fe27ac53ff06af51fa4
Kamal Yusuf <kbomoi@yahoo.com>
2022-09-14 10:47:00 +0300

    1

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { useMemo } from "react";
+import { useRouter } from "next/router";
+
+export const useQueryParams = <T extends string>(
+  paths: T[]
+): { [K in T]?: string } => {
+  const { query } = useRouter();
+
+  return useMemo(() => {
+    return paths.reduce((params, path) => {
+      return {
+        ...params,
+        [path]: typeof query[path] === "string" ? query[path] : undefined,
+      };
+    }, {});
+  }, [paths, query]);
+};
