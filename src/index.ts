0897c8dcf0be37f6428c6abd143f3fe2151fa930
Kamal <kbomoi@yahoo.com>
2023-05-28 00:46:39 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,4 +16,18 @@ export const useQueryParams = <T extends string>(
   }, [paths, query]);
 };
 
-export const useTypedQueryParams = useQueryParams;
+export const useTypedQueryParams = <T extends string>(
+  paths: T[]
+): { [K in T]?: string } => {
+  const { query } = useRouter();
+
+  return useMemo(() => {
+    return paths.reduce((params, path) => {
+      return {
+        ...params,
+        [path]: typeof query[path] === "string" ? query[path] : undefined,
+      };
+    }, {});
+  }, [paths, query]);
+};
+
